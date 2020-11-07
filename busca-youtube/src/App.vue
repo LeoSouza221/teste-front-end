099db215b3daeea630d8c25e7da971e0ecf2e62c
LeoSouza221 <leosouza221@gmail.com>
2020-11-07 15:41:56 -0300

    Criado configuracoes iniciais do projeto

---
+++ busca-youtube/src/App.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  v-app
+    v-fade-transition(mode="out-in")
+      router-view
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ busca-youtube/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-main(
+    :style="{backgroundColor: $vuetify.theme.themes.dark.primary}"
+  )
+    v-container
+      BuscarVideos
+</template>
+
+<script>
+import BuscarVideos from './BuscarVideos.vue';
+
+export default {
+  name: 'Home',
+
+  components: {
+    BuscarVideos,
+  },
+
+  created() {
+    console.log(this.$vuetify);
+  },
+};
+</script>
